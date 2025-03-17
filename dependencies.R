d5fd8d6367202b72dea2e7c71c3870c6e4cb4923
PraneethaReddy02 <praneereddy02@gmail.com>
2025-03-17 15:20:37 +0530

    Create dependencies.R

---
+++ dependencies.R
@@ -0,0 +1,25 @@
+# dependencies.R
+
+# List of required packages
+required_packages <- c("shiny", "quantmod", "xts")
+
+# Install any missing packages
+new_packages <- required_packages[!(required_packages %in% installed.packages()[, "Package"])]
+if(length(new_packages)) install.packages(new_packages)
+
+# Load the required packages
+lapply(required_packages, library, character.only = TRUE)
+
+# Define a vector of Nifty50 companies (Yahoo Finance tickers)
+companies <- c(
+  "Reliance Industries" = "RELIANCE.NS",
+  "Tata Consultancy Services" = "TCS.NS",
+  "HDFC Bank" = "HDFCBANK.NS",
+  "Infosys" = "INFY.NS",
+  "ICICI Bank" = "ICICIBANK.NS",
+  "Kotak Mahindra Bank" = "KOTAKBANK.NS",
+  "Hindustan Unilever" = "HINDUNILVR.NS",
+  "State Bank of India" = "SBIN.NS",
+  "Bajaj Finance" = "BAJFINANCE.NS",
+  "Bharti Airtel" = "BHARTIARTL.NS"
+)
